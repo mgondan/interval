c2a2c49e0e3e2c0b6f2cbc763a51b4fefa09a2f8
Matthias Gondan <Matthias.Gondan-Rochon@uibk.ac.at>
2024-06-23 13:22:18 +0200

    add unit tests for rint

---
+++ test/test_rint.pl
@@ -0,0 +1,20 @@
+:- module(test_rint, [test_rint/0]).
+
+:- use_module(library(plunit)).
+:- use_module(library(debug)).
+:- use_module(library(rint)).
+
+test_interval :-
+    run_tests([binom]).
+
+:- begin_tests(binom).
+
+test(dbinom1) :-
+    interval(dbinom(11...12, 20...21, 0.6...0.7), L...U),
+    L > 0.04118,
+    L < 0.04119,
+    U > 0.17970,
+    U < 0.17971.
+
+:- end_tests(binom).
+
