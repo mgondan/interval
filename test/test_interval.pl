b13ebd60aabd80382a9f234b322cf23865771eee
mgondan <Matthias.Gondan@psy.ku.dk>
2024-06-25 10:11:41 +0200

    unit tests for sqrt

--- test/test_interval.pl
+++ test/test_interval.pl
@@ -9,7 +9,7 @@
 :- set_prolog_flag(float_zero_div, infinity).
 
 test_interval :-
-    run_tests([comparison]).
+    run_tests([comparison, sqrt]).
 
 :- begin_tests(comparison).
 
@@ -23,71 +23,74 @@ test(=<) :-
     B = 2...4,
     interval(A =< B, true).
 
+:- end_tests(comparison).
+
+:- begin_tests(sqrt).
+
 test(sqrt1) :-
     A = 1...2,
     interval(sqrt(A), L...U),
     L > 0.99999,
     L < 1.00001,
-    U > 1.41429,
-    U < 1.41430.
+    U > 1.41421,
+    U < 1.41422.
 
 test(sqrt2) :-
     A = 0...2,
     interval(sqrt(A), L...U),
     L = 0.0,
-    U > 1.41429,
-    U < 1.41430.
+    U > 1.41421,
+    U < 1.41422.
 
 test(sqrt3) :-
     A = -1...2,
     interval(sqrt(A), L...U),
     L = 1.5NaN,
-    U > 1.41429,
-    U < 1.41430.
+    U > 1.41421,
+    U < 1.41422.
 
 test(sqrt4) :-
     A = -2...0,
     interval(sqrt(A), L...U),
-    L = 0.0,
+    L = 1.5NaN,
     U = 0.0.
 
 test(sqrt5) :-
     A = -2... -1,
     interval(sqrt(A), X),
-    X = 1.5NaN.
+    X = 1.5NaN...1.5NaN.
 
 test(sqrt6) :-
     A = 1...2,
     interval(sqrt1(A), L...U),
     L > 0.99999,
     L < 1.00001,
-    U > 1.41429,
-    U < 1.41430.
+    U > 1.41421,
+    U < 1.41422.
 
 test(sqrt7) :-
     A = 0...2,
     interval(sqrt1(A), L...U),
     L = 0.0,
-    U > 1.41429,
-    U < 1.41430.
+    U > 1.41421,
+    U < 1.41422.
 
 test(sqrt8) :-
     A = -1...2,
     interval(sqrt1(A), L...U),
     L = 0.0,
-    U > 1.41429,
-    U < 1.41430.
+    U > 1.41421,
+    U < 1.41422.
 
 test(sqrt9) :-
     A = -2...0,
-    interval(sqrt1(A), L...U),
-    L = 0.0,
-    U = 0.0.
+    interval(sqrt1(A), X),
+    X = 1.5NaN...0.0.
 
 test(sqrt10) :-
     A = -2... -1,
     interval(sqrt1(A), X),
-    X = 0.0.
+    X = 1.5NaN.
 
-:- end_tests(comparison).
+:- end_tests(sqrt).
 
