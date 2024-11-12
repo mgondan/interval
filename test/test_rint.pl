3484b531b704db659fbce1179024b0517ff1b646
Jeremy Irilli <jeremy.irilli@student.uibk.ac.at>
2024-11-12 09:21:38 +0100

    Updated tests of rint with helper predicate

--- test/test_rint.pl
+++ test/test_rint.pl
@@ -10,127 +10,97 @@ test_rint :-
 :- begin_tests(binom).
 
 test(dbinom1) :-
-    interval(dbinom(11...12, 20...21, 0.6...0.7), L...U),
-    L > 0.04118,
-    L < 0.04119,
-    U > 0.17970,
-    U < 0.17971.
+    interval(dbinom(11...12, 20...21, 0.6...0.7), Res),
+    equal(Res, 0.0411...0.1798).
 
 % left to X / N
 test(dbinom2) :- 
-    interval(dbinom(10...11, 20...21, 0.6...0.7), L...U),
-    L > 0.01764,
-    L < 0.01765,
-    U > 0.15973,
-    U < 0.15974.
+    interval(dbinom(10...11, 20...21, 0.6...0.7), Res),
+    equal(Res, 0.0176...0.1598).
 
 % right to X / N
 test(dbinom3) :- 
-    interval(dbinom(15...16, 20...21, 0.6...0.7), L...U),
-    L > 0.03499,
-    L < 0.03500,
-    U > 0.18780,
-    U < 0.18781.
+    interval(dbinom(15...16, 20...21, 0.6...0.7), Res),
+    equal(Res, 0.0349...0.1879).
 
 test(qbinom_lowertail) :-
-    interval(qbinom(0.5...0.6, 20...21, 0.6...0.7, atomic(true)), L...U),
-    L > 11.99999,
-    L < 12.00001,
-    U > 14.99999,
-    U < 15.00001.
+    interval(qbinom(0.5...0.6, 20...21, 0.6...0.7, atomic(true)), Res),
+    equal(Res, 12...15).
 
 test(qbinom_uppertail) :-
-    interval(qbinom(0.5...0.6, 20...21, 0.6...0.7, atomic(false)), L...U),
-    L > 10.99999,
-    L < 11.00001,
-    U > 14.99999,
-    U < 15.00001.
+    interval(qbinom(0.5...0.6, 20...21, 0.6...0.7, atomic(false)), Res),
+    equal(Res, 11...15).
 
 test(pbinom_lowertail) :-
-    interval(pbinom(10...11, 20...21, 0.6...0.7, atomic(true)), L...U),
-    L > 0.02638,
-    L < 0.02639,
-    U > 0.40440,
-    U < 0.40441.
+    interval(pbinom(10...11, 20...21, 0.6...0.7, atomic(true)), Res),
+    equal(Res, 0.0263...0.4045).
 
 test(pbinom_uppertail) :-
-    interval(pbinom(10...11, 20...21, 0.6...0.7, atomic(false)), L...U),
-    L > 0.59559,
-    L < 0.59560,
-    U > 0.97361,
-    U < 0.97362.
+    interval(pbinom(10...11, 20...21, 0.6...0.7, atomic(false)), Res),
+    equal(Res, 0.5955...0.9737).
 
 :- end_tests(binom).
 
 :- begin_tests(normal).
 
 test(pnorm) :-
-    interval(pnorm(90...91, 100...101, 10...11), L...U),
-    L > 0.13566,
-    L < 0.13567,
-    U > 0.20662,
-    U < 0.20663.
+    interval(pnorm(90...91, 100...101, 10...11), Res),
+    equal(Res, 0.1356...0.2067).
 
 test(qnorm) :-
-    interval(qnorm(0.6...0.7, 100...101, 10...11), L...U),
-    L > 102.53347,
-    L < 102.53348,
-    U > 106.76840,
-    U < 106.76841.
+    interval(qnorm(0.6...0.7, 100...101, 10...11), Res),
+    equal(Res, 102.5334...106.7685).
 
 test(dnorm_z_neg) :-
-    interval(dnorm(90...91, 100...101, 10...11), L...U),
-    L > 0.01980,
-    L < 0.01981,
-    U > 0.02854,
-    U < 0.02855.
+    interval(dnorm(90...91, 100...101, 10...11), Res),
+    equal(Res, 0.0198...0.0286).
 
 test(dnorm_z_pos) :-
-    interval(dnorm(110...111, 100...101, 10...11), L...U),
-    L > 0.01980,
-    L < 0.01981,
-    U > 0.02854,
-    U < 0.02855.
+    interval(dnorm(110...111, 100...101, 10...11), Res),
+    equal(Res, 0.0198...0.0286).
 
 test(dnorm_z_mixed) :-
-    interval(dnorm(99...102, 100...101, 10...11), L...U),
-    L > 0.03554,
-    L < 0.03555,
-    U > 0.03989,
-    U < 0.03990.
+    interval(dnorm(99...102, 100...101, 10...11), Res),
+    equal(Res, 0.0355...0.0399).
 
 :- end_tests(normal).
 
 :- begin_tests(t).
 
-test(pt_lowertail) :-
-    interval(pt(0.5...1, atomic(5), atomic(true)), L...U),
-    L > 0.6808,
-    U < 0.8184.
+test(pt_lowertail_neg) :-
+    interval(pt(-0.5... -0.2, atomic(5), atomic(true)), Res),
+    equal(Res, 0.3191...0.4247).
 
-test(pt_uppertail) :-
-    interval(pt(0.5...1, atomic(5), atomic(false)), L...U),
-    L > 0.1816,
-    U < 0.3192.
+test(pt_lowertail_pos) :-
+    interval(pt(0.5...1, atomic(5), atomic(true)), Res),
+    equal(Res, 0.6808...0.8184).
+
+test(pt_uppertail_neg) :-
+    interval(pt(-0.5... -0.2, atomic(5), atomic(false)), Res),
+    equal(Res, 0.5753...0.6809).
+
+test(pt_uppertail_pos) :-
+    interval(pt(0.5...1, atomic(5), atomic(false)), Res),
+    equal(Res, 0.1816...0.3192).
 
 test(qt) :-
-    interval(qt(0.4...0.6, atomic(5)), L...U),
-    L > -0.2672,
-    U < 0.2672.
+    interval(qt(0.4...0.6, atomic(5)), Res),
+    equal(Res, -0.2672...0.2672).
 
 test(dt_neg) :-
-    interval(dt(-0.5... -0.4, atomic(5)), L...U),
-    L > -0.3280,
-    U < 0.3454.
+    interval(dt(-0.5... -0.4, atomic(5)), Res),
+    equal(Res, 0.3279...0.3454).
 
 test(dt_pos) :-
-    interval(dt(0.4...0.5, atomic(5)), L...U),
-    L > 0.3279,
-    U < 0.3454.
+    interval(dt(0.4...0.5, atomic(5)), Res),
+    equal(Res, 0.3279...0.3454).
 
 test(dt_mixed) :-
-    interval(dt(-0.4... 0.5, atomic(5)), L...U),
-    L > 0.3279,
-    U < 0.3797.
+    interval(dt(-0.4... 0.5, atomic(5)), Res),
+    equal(Res, 0.3279...0.3797).
 
 :- end_tests(t).
+
+% Helper predicate to check equality
+equal(Res0, Res) :-
+    interval:interval(round(Res0, atomic(4)), Res).
