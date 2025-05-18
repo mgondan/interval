ab6df576e885dfaa2e7e723c355703e7b2eb55b9
Jeremy Irilli <jeremy.irilli@student.uibk.ac.at>
2025-05-18 16:12:27 +0200

    Add wrapper r/1 and r/2 to rint.pl R package

--- inst/prolog/rint.pl
+++ inst/prolog/rint.pl
@@ -18,25 +18,10 @@
 
 :- consult([lib/interface, lib/core, lib/op, lib/rint_op]).
 
-/** <module> Use intervals in R functions.
+% Call R
+r(Expr)
+ => rolog:r_call(Expr).
 
-This module expands the 'interval' module with R functions.
-For general information on the use of interval/2, please refer to that module. 
- */
-
-% Binomial distribution
-% - Cumulated density lower-tail: interval(pbinom(X, N, P, true), Res)
-% - Cumulated density upper-tail: interval(pbinom(X, N, P, false), Res)
-% - Quantile: interval(qbinom(Alpha, N, P, true), Res)
-% - Density: interval(dbinom(X, N, P), Res)
-%
-% Normal distribution
-% - Cumulated density: interval(pnorm(X, Mu, Sigma), Res)
-% - Quantile: interval(qnorm(P, Mu, Sigma), Res)
-% - Density: interval(dnorm(X, Mu, Sigma), Res)
-%
-% T distribution
-% - Cumulated density lower-tail: interval(pt(X, Df, true), Res)
-% - Cumulated density upper-tail: interval(pt(X, Df, false), Res)
-% - Quantile: interval(qt(P, Df), Res)
-% - Density: interval(dt(X, Df), Res)
+% Evaluate R expression
+r(Expr, Res)
+ => rolog:r_eval(Expr, Res).
