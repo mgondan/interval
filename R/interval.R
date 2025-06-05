eea18a0c91fe490bc53c4e754566f11b9f7d175d
Gondan-Rochon <c7201178@uibk.ac.at>
2025-06-05 18:46:09 +0200

    Allow for call("...", 1.1, 1.2) as input

--- R/interval.R
+++ R/interval.R
@@ -16,7 +16,18 @@
 #' @return
 #' The numeric result of the expression.
 #' @export
-interval <- function(expr, flags = NULL, env = globalenv()) {
+interval <- function(expr, flags = NULL, env = globalenv()) 
+{
+  if(is.character(expr))
+    return(.interval.string(expr, flags, env))
+
+  flags <- c(flags, list(cat = FALSE))
+  t <- rolog::once(call("interval", expr, expression(X), flags), env = env)
+  return(t$X)
+}
+
+# For easy testing, we allow for string expressions
+.interval.string <- function(expr, flags = NULL, env = globalenv()) {
   expr <- rolog::once(call("term_string", expression(T), expr))
   flags <- c(flags, list(cat = FALSE))
   t <- rolog::once(call("interval", expr$T, expression(X), flags), env = env)
