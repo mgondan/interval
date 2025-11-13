296565f0e8df5b2ecf758aa08b3d5cd202239c0f
jeremyirilli <149394139+jeremyirilli@users.noreply.github.com>
2025-11-13 21:22:34 +0100

    Update unit test round R package

--- tests/testthat/test-round.R
+++ tests/testthat/test-round.R
@@ -3,7 +3,12 @@ test_that("round1", {
   expect_equal(eval(expr), ...(1.58, 2.17))
 })
 
-test_that("round2interval", {
-  expr <- quote(round2interval(...(1.582, 2.168), digits=2))
-  expect_equal(round2interval(2.135, digits=2), ...(2.13, 2.14))
+test_that("round2interval_1", {
+  expr <- quote(round2interval(2.135, digits=2))
+  expect_equal(eval(expr), ...(2.13, 2.14))
+})
+
+test_that("round2interval_2", {
+  expr <- quote(round2interval(2.1, digits=2))
+  expect_equal(eval(expr), ...(2.09, 2.11))
 })
