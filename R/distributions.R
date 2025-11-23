85afcc6aa514564834c881eaf209dcdee01864a2
Jeremy Irilli <jeremy.irilli@student.uibk.ac.at>
2025-11-23 21:18:36 +0100

    Add qbinom, dbinom R package

--- R/distributions.R
+++ R/distributions.R
@@ -1,12 +1,17 @@
 #' @title 
 #' The Binomial Distribution
-#'
+#' 
+#' @name Binomial
+#' 
 #' @description
 #' For more information, refer to the documentation
 #' of [dbinom()], [pbinom()], [qbinom()] from `stats`.
 #' 
 #' @param x
 #' Number of successes
+#' 
+#' @param p
+#' probability 
 #' 
 #' @param size
 #' Number of trials
@@ -25,7 +30,9 @@
 #' The numeric result as interval or number.
 #' 
 #' @examples 
+#' dbinom(...(5, 6), ...(20, 21), ...(0.3, 0.5))
 #' pbinom(...(5, 6), ...(20, 21), ...(0.3, 0.5))
+#' qbinom(...(0.5, 0.6), ...(20, 21), ...(0.3, 0.5))
 #' @export
 #' @md
 pbinom <- function(x, size, prob, lower.tail = TRUE, ...) {
@@ -38,4 +45,30 @@ pbinom <- function(x, size, prob, lower.tail = TRUE, ...) {
 
 .pbinom_default <- function(x, size, prob, lower.tail = TRUE, ...) {
   stats::pbinom(x, size, prob, lower.tail, ...)
+}
+
+#' @rdname Binomial
+qbinom <- function(p, size, prob, lower.tail = TRUE, ...) {
+  .dispatch("qbinom", p, size, prob, lower.tail, ...)
+}
+
+.qbinom_interval <- function(p, size, prob, lower.tail = TRUE, ...) {
+  .eval("qbinom", p, size, prob, lower.tail)
+}
+
+.qbinom_default <- function(p, size, prob, lower.tail = TRUE, ...) {
+  stats::qbinom(p, size, prob, lower.tail, ...)
+}
+
+#' @rdname Binomial
+dbinom <- function(x, size, prob, ...) {
+  .dispatch("dbinom", x, size, prob, ...)
+}
+
+.dbinom_interval <- function(x, size, prob, ...) {
+  .eval("dbinom", x, size, prob)
+}
+
+.dbinom_default <- function(x, size, prob, ...) {
+  stats::dbinom(x, size, prob, ...)
 }
--- tests/testthat/test-distributions.R
+++ tests/testthat/test-distributions.R
@@ -22,3 +22,53 @@ test_that("pbinom 5", {
   expr <- quote(pbinom(...(6, 8), ...(20, 22), ...(0.3, 0.4)))
   expect_equal(eval(expr), ...(0.15844366131574084, 0.8866685371230216))
 })
+
+test_that("qbinom 1", {
+  expr <- quote(qbinom(0.5, 20, 0.2))
+  expect_equal(eval(expr), 4.0)
+})
+
+test_that("qbinom 2", {
+  expr <- quote(qbinom(...(0.5, 0.7), 20, 0.2))
+  expect_equal(eval(expr), ...(4.0, 5.0))
+})
+
+test_that("qbinom 3", {
+  expr <- quote(qbinom(0.5, ...(20, 24), 0.2))
+  expect_equal(eval(expr), ...(4.0, 5.0))
+})
+
+test_that("qbinom 4", {
+  expr <- quote(qbinom(0.5, 20, ...(0.2, 0.4)))
+  expect_equal(eval(expr), ...(4.0, 8.0))
+})
+
+test_that("qbinom 5", {
+  expr <- quote(qbinom(...(0.5, 0.7), ...(20, 22), ...(0.2, 0.4)))
+  expect_equal(eval(expr), ...(4.0, 10.0))
+})
+
+test_that("dbinom 1", {
+  expr <- quote(dbinom(6, 20, 0.4))
+  expect_equal(eval(expr), 0.1244117)
+})
+
+test_that("dbinom 2", {
+  expr <- quote(dbinom(...(6, 8), 20, 0.3))
+  expect_equal(eval(expr), ...(0.11439673970486122, 0.19163898275344257))
+})
+
+test_that("dbinom 3", {
+  expr <- quote(dbinom(6, ...(20, 21), 0.3))
+  expect_equal(eval(expr), ...(0.1878062030983737, 0.19163898275344257))
+})
+
+test_that("dbinom 4", {
+  expr <- quote(dbinom(6, 20, ...(0.3, 0.5)))
+  expect_equal(eval(expr), ...(0.03696441650390632, 0.19163898275344257))
+})
+
+test_that("dbinom 5", {
+  expr <- quote(dbinom(...(6, 8), ...(20, 22), ...(0.3, 0.4)))
+  expect_equal(eval(expr), ...(0.08621730755584592, 0.19163898275344257))
+})
