98e716a05e432acaa85ce78948d35aff433c1740
mgondan <Matthias.Gondan-Rochon@uibk.ac.at>
2026-01-03 11:06:42 +0100

    Update utility.pl

--- inst/prolog/lib/utility.pl
+++ inst/prolog/lib/utility.pl
@@ -103,8 +103,7 @@ arg_list(A...A, List) :-
 arg_list(L...U, List) :- 
     !, List = [L, U].
 
-arg_list(A, List) :- 
-    !, List = [A].
+arg_list(A, [A]).
 
 list_hashtag(List, Term) :-
     compound_name_arguments(Term, #, List).
